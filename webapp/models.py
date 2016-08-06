1479e8f1f8b028e1c7e394ec6da02f5f3e8cee51
Christian Howard <christian.john.howard@gmail.com>
2016-08-06 16:29:22 -0400

    Basic Model Structure and File Setup.

---
+++ webapp/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class Player(models.Model):
+    pid = models.IntegerField(primary_key=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    birthdate = models.DateField()
+    occupation = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+
+class Season(models.Model):
+    sid = models.IntegerField(primary_key=True)
+    seasonname = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    #fsd is film start date
+    fsd = models.DateField()
+    #fed is film end date
+    fed = models.DateField()
+    #asd is air start date
+    asd = models.DateField()
+    #aed is air end date
+    aed = models.DateField()
+    
+class PxS(models.Model):
+    # Do I need a primary key on this? PROBABLY -- One to many relationship: one player, potential multiple seaons
+    pid = models.ForeignKey('Player', on_delete = models.CASCADE,)
+    sid = models.ForeignKey('Season', on_delete = models.CASCADE,)
+    # position they finished in
+    finishposition = models.IntegerField()
+    # total number of players that season
+    totalpositions = models.IntegerField()
+    
+class Statistics(models.Model):
+    pid = models.ForeignKey('Player', on_delete = models.CASCADE,)
+    totalseasons = models.IntegerField()
+    totaldays = models.IntegerField()
+    tribalwins = models.IntegerField()
+    individualwins = models.IntegerField()
+    totalwins = models.IntegerField()
+    # total votes received against the player
+    totalvotesreceived = models.IntegerField()
+    daysonexile = models.IntegerField()
+    daysonredemption = models.IntegerField()
+    # total duels won on Redemption Island
+    totalduelswon = models.IntegerField()
+    
