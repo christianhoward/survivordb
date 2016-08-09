9a875fa73778824163a16c05fabf5e37fbb44364
Christian Howard <christian.john.howard@gmail.com>
2016-08-09 18:49:23 -0400

    Added additional url patterns and views.

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^player/(?P<pk>\d+)/$', views.player, name='player'),
+    url(r'^season/(?P<pk>\d+)/$', views.season, name='season'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from .models import Player
+from .models import Player, Season
 
 def home(request):
     players = Player.objects.order_by('pid')
     return render(request, 'webapp/home.html', {'players': players})
+
+def player(request):
+    players = Player.objects.order_by('pid')
+    return render(request, 'webapp/player.html', {'players': players})
+    #make player.html
+    
+def season(request):
+    seasons = Season.objects.order_by('sid')
+    return render(request, 'webapp/season.html', {'seasons': seasons})
+    #make season.html
