32348c03c54d74247c2cd33e707b265d8a26a0fe
Christian Howard <christian.john.howard@gmail.com>
2016-08-29 17:26:26 -0400

    Created jQuery sorting functions

--- webapp/static/js/webapp.js
+++ webapp/static/js/webapp.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $('.pull-me').click(function() {
+    $('.togglespoilers').click(function() {
         $('.panel').slideToggle('slow');
-        });
     });
+    
+    var $divs = $('div.col-xs-3');
+    $('.alpha').on('click', function () {
+        var alphabeticallyOrderedDivs = $divs.get().sort(function (a, b) {
+            return $(a).attr('data-name').localeCompare($(b).attr('data-name'));
+        });
+        $('.people').empty().append(alphabeticallyOrderedDivs);
+    });
+    
+    $('.finish').on('click', function () { 
+        var numericallyOrderedDivs = $divs.get().sort(function (a, b) { 
+            return $(a).attr('data-id') < $(b).attr('data-id'); 
+        });
+        $('.people').empty().append(numericallyOrderedDivs);
+    });
+    
+});
